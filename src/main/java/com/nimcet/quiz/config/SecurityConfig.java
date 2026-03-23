a167ce461ad070cde3de9536d6ba7abacd05ce77
Arpit0034 <kandariarpit096@gmail.com>
2026-03-23 08:52:57 +0530

    update

--- src/main/java/com/nimcet/quiz/config/SecurityConfig.java
+++ src/main/java/com/nimcet/quiz/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(url,"http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://nimcetmaster.netlify.app",url));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
